359f1a7ed3f1e611d9d1a3ed1dc7d0ba9efd8b94
NikhilPatil0024 <nikhil.k.patil2004@gmail.com>
2025-05-14 07:06:36 +0530

    Add files via upload

---
+++ inputFunc.py
@@ -0,0 +1,18 @@
+a = input()
+print(a)
+
+b = input("Enter Your Name: ")
+print(b)
+
+# Input do takes an argument as String in Default. 
+# got to do typeCast if wanting to do the operations on it.
+
+x = float(input("Enter First Number: "))
+y = float(input("Enter Second Number: "))
+print(x + y)
+
+q = input("Enter First Number: ")
+p = input("Enter Second Number: ")
+print(int(q) + int(p))
+
+# can be convert into others data types as well
