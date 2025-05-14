b2415264b119b3270923e71c623ff2eff888969d
NikhilPatil0024 <nikhil.k.patil2004@gmail.com>
2025-05-14 07:07:00 +0530

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,11 @@
+# Arithmetic Operations using input functions
+
+a = float(input("enter the first number a: "))
+b = float(input("enter the second number b: "))
+print("Sum:\n", a + b)
+print("Sub:\n", a - b)
+print("Multi:\n", a * b)
+print("Divide:\n", a / b)
+print("floor division:\n", a // b)
+print("Modulo:\n", a % b)
+print("exponent:\n" , a ** b)
