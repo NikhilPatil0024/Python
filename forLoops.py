8e50954d92ff70c6ad46af2ea9e89f84b7da8aba
Nikhil Patil <211445300+NikhilPatil0024@users.noreply.github.com>
2025-06-01 23:15:34 +0530

    Add files via upload

---
+++ forLoops.py
@@ -0,0 +1,19 @@
+name = 'Nikhil'
+for item in name:
+    print(item) 
+
+colors = ["Red", "Blue", "Green", "Yellow", "Purple"]
+for color in colors:
+    print(color)
+
+for items in range(1, 201):
+    print(items)
+
+
+for i in range(1, 11, 2):
+    print(i)
+
+num = 0
+for i in range(10):
+    num = (num+5)
+    print(num)
---
+++ whileLoops.py
@@ -0,0 +1,12 @@
+number = 5
+
+while (number < 51):
+    print(number)
+    number = number + 5
+
+print("Done!")
+
+count = 5
+while (count > 0):
+    print(count)
+    count -= 1
