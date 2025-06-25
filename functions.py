feca6e439efb6f7014cf1ddc5fba85a8bb8b5917
Nikhil Patil <211445300+NikhilPatil0024@users.noreply.github.com>
2025-06-25 17:36:30 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,41 @@
+
+
+a = int(input("Enter the number 'a': "))
+b = int(input("Enter the number 'b': "))
+
+
+def calculateGmean(a, b):
+    gmean = (a*b) / (a+b)
+    print(gmean)
+
+calculateGmean(a,b)
+
+def greaterNo(a,b):
+    if (a > b):
+        print("First Number is Greater.")
+    elif (a == b): 
+        print("Both are equal.")
+    else:
+        print("Second Number is Greater")
+greaterNo(a, b)
+
+
+def islesser(a ,b):
+    pass
+
+# Built in Functions.
+# Eg:
+name = "Nikhil"
+roll = 21
+min(a, b )
+max(a, b)
+len(name)
+# sum()
+type(name)
+range(a)
+dict({name: "Krish", roll: 21})
+tuple()
+set()
+print()
+# etc.
+
