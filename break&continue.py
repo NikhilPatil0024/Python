6caa2c547d7a4e734a80593489cd9b20619268ee
Nikhil Patil <211445300+NikhilPatil0024@users.noreply.github.com>
2025-06-25 17:05:08 +0530

    Add files via upload

---
+++ break&continue.py
@@ -0,0 +1,27 @@
+# breaks = leave the loop.
+# continue = leave the iteration.
+
+# greet = ""
+# for greet in range(10):
+#     print('Hey!')
+#     if greet == 5: 
+#         break
+
+# for i in range(10):
+#     i += 1
+#     print("5 x ", i, "=",  5 * i )
+#     if i == 5:
+#         break
+
+j = 0
+x = 0
+while j in range(10): 
+    j = j + 1
+    x = x + 1
+    print(f"5 x {x} = " ,j * 5)
+    if j == 10:
+        break 
+
+
+
+
